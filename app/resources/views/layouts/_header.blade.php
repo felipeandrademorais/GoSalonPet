01490b5912cd2b2de31e57c500f8b6d92e3b4f2d
Felipe Morais <felipemorais@inovadora.com.br>
2021-02-27 21:35:18 -0300

    Ajustes iniciais

---
+++ app/resources/views/layouts/_header.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <img id="ico" src=""/>
+    <div id="menu">
+        <ul>
+            <li> <a href="{{ url('/') }}"> Home </a> </li>
+            <li>  <a href="{{ url('/') }}"> Nossos Serviços </a> </li>
+            <li> <ico /> </li>
+            <li> <ico /> </li>
+            <li>
+                <ico />
+                 <a href="{{ url('/') }}">  </a> 
+            </li>
+        </ul>
+    </div>
+
+</div>
